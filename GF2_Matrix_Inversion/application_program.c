6669fa72623b7d4ba5102e27f2d7169f55e88a78
Dhruva Hegde <53150130+Intelectron6@users.noreply.github.com>
2023-07-08 20:58:01 +0530

    Update application_program.c

--- GF2_Matrix_Inversion/application_program.c
+++ GF2_Matrix_Inversion/application_program.c
@@ -12,35 +12,29 @@ int main()
 	int output_vector;
 
 	////Test case 1: Input = CA31 => Output = 7F31
-
 	input_vector = 0xFFFFFFFF;
 	extra_inputs=0x0;
 	IOWR(USER_INPUT_0_BASE, 0, input_vector);
 	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
-	usleep(1); //1 us delay
 
 	input_vector = 0x0000CA31;
 	extra_inputs=0x1;
 	IOWR(USER_INPUT_0_BASE, 0, input_vector);
 	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
-	usleep(1); //1 us delay
 	printf("Sending input to HW %d\n", input_vector);
 
 	input_vector = 0x00000001;
 	extra_inputs=0x0;
 	IOWR(USER_INPUT_0_BASE, 0, input_vector);
 	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
-	usleep(1); //1 us delay
 
 	extra_inputs=0xF;
 	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
 
 	while (1)
 	{
-		usleep(1); //1 us delay
 		output_vector = IORD(USER_OUTPUT_BASE, 0);
 		IOWR(USER_INPUT_1_BASE, 0, output_vector);
-		usleep(1); //1 us delay
 		if (output_vector == 0x00000001) break;
 	}
 
@@ -49,7 +43,41 @@ int main()
 	output_vector = IORD(USER_OUTPUT_BASE, 0);
 	printf("Received output from HW %d\n", output_vector);
 
+	usleep(1); //1 us delay
+
+	////Test case 2: Input = 8421 => Output = 8421
+	input_vector = 0xFFFFFFFF;
+	extra_inputs=0x0;
+	IOWR(USER_INPUT_0_BASE, 0, input_vector);
+	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
+
+	input_vector = 0x00008421;
+	extra_inputs=0x1;
+	IOWR(USER_INPUT_0_BASE, 0, input_vector);
+	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
+	printf("Sending input to HW %d\n", input_vector);
+
+	input_vector = 0x00000001;
+	extra_inputs=0x0;
+	IOWR(USER_INPUT_0_BASE, 0, input_vector);
+	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
+
+	extra_inputs=0xF;
+	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
+
+	while (1)
+	{
+		output_vector = IORD(USER_OUTPUT_BASE, 0);
+		IOWR(USER_INPUT_1_BASE, 0, output_vector);
+		if (output_vector == 0x00000001) break;
+	}
+
+	extra_inputs=0x2;
+	IOWR(USER_EXTRA_INPUTS_BASE, 0, extra_inputs);
+	output_vector = IORD(USER_OUTPUT_BASE, 0);
+	printf("Received output from HW %d\n", output_vector);
+	
+	usleep(1); //1 us delay
 	IOWR(USER_INPUT_1_BASE, 0, 0xFFFFFFFF);
 	return 0;
 }
-
